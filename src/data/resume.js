581641b08c3b71276c381b103a52019320bc2343
gargavi-oss <gargavi2006@outlook.com>
2026-06-27 17:51:43 +0530

    fixed bug

--- src/data/resume.js
+++ src/data/resume.js
@@ -1,9 +1,9 @@
-import resumeJson from "../data/data.json"  
-
+import resumeJson from "../data/resume.json"
+  
 export const profile = resumeJson.profile;
 export const about = resumeJson.about
 export const education = resumeJson.education
 export const projects = resumeJson.projects
 export const achievements = resumeJson.achievements
 export const skills = resumeJson.skills
-export const socials = resumeJson.skills
+export const socials = resumeJson.socials
